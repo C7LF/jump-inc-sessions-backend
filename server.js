2c1a18fb93a5e1c1446515ac230d0449a666d3d9
C7LF <ccal72@yahoo.co.uk>
2020-08-02 23:46:32 +0100

    added npm cors

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 const puppeteer = require('puppeteer');
 
